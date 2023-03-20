93aca23114c994505fe935963a7b6f527727fc9e
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2023-03-20 16:20:10 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 import { Observable } from 'rxjs'
+import { filter, map } from 'rxjs/operators'
+
+
+const status = {
+    ACTIVE: "active",
+    INACTIVE: "inactive"
+}
+
+const users = {
+    data: [
+        {
+            status: status.ACTIVE,
+            age: 18,
+        },
+        {
+            status: status.INACTIVE,
+            age: 15,
+        },
+        {
+            status: status.INACTIVE,
+            age: 100,
+        },
+        {
+            status: status.INACTIVE,
+            age: 25,
+        },
+        {
+            status: status.ACTIVE,
+            age: 32,
+        },
+        {
+            status: status.ACTIVE,
+            age: 30,
+        },
+        {
+            status: status.ACTIVE,
+            age: 18,
+        }
+    ]
+}
 
 const observable = new Observable(subscriber =>
-    subscriber.next('Hello World')
+    subscriber.next(users)
+).pipe(
+    map(({ data }) => data),
+    map(data => data.filter(item => item.status === status.ACTIVE)),
+    map(data => {
+        let avg = data.reduce((sum, user) => sum + user.age, 0) / data.length
+        if (avg < 18) throw new Error("Too Young")
+        return data
+    }),
+    // map(data => data.)
+
 )
 
 const observer = {
@@ -9,7 +59,7 @@ const observer = {
         console.log("Emmiting value: ", value);
     },
     error: error => {
-        console.log("Error: ", error);
+        console.log("Error: ", error.message);
     },
     complete: () => {
         console.log("Completed");
